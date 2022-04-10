764dd745bd0ec0490139d032e3cb10c901881269
Jkey-u <dum6894@gmail.com>
2022-04-10 20:25:47 +0900

    #7 API Routes, 로그인 구현

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -10,6 +10,8 @@ export default function Gnb() {
       activeItem = 'home';
     } else if(router.pathname === '/about') {
       activeItem = 'about';
+    } else if(router.pathname === '/admin') {
+      activeItem = 'admin';
     }
 
     const goLink = (e, data) => {
@@ -38,6 +40,13 @@ export default function Gnb() {
           active={activeItem === "contact"}
           onClick={() => {
             router.push("/contact");
+          }}
+        />
+        <Menu.Item
+          name="Admin"
+          active={activeItem === "admin"}
+          onClick={() => {
+            router.push("/admin");
           }}
         />
       </Menu>
